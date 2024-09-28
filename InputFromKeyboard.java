57d18ca9f76b4ed28fb8c84e6a65ae924f2ffc33
Github1byNz <81811176+Github1byNz@users.noreply.github.com>
2024-09-28 23:57:12 +0700

    4 in a row

---
+++ InputFromKeyboard.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class InputFromKeyboard {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What's your name? ");
+        String strName = scanner.nextLine();
+        System.out.println("How old are your? ");
+        int age = scanner.nextInt();
+        System.out.println("How tall are you (m)? ");
+        double height = scanner.nextDouble();
+        System.out.println("Mrs/Ms. " + strName + ", " + age + " years old. " + "Your height is: " + height + "m.");
+        scanner.close();
+        System.exit(0);
+    }
+}
